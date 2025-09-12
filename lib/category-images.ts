05f125bcd3aaaf9b37c4f75a96dbd863e18b7b4a
dlwjddhks <67868522+dlwjddhks@users.noreply.github.com>
2025-09-12 14:22:22 +0900

    지도 사진 추가

---
+++ lib/category-images.ts
@@ -0,0 +1,10 @@
+// app/lib/category-images.ts
+export const CATEGORY_IMAGE: Record<string, string> = {
+    "한식": "/images/categories/korean.jpg",
+    "중식": "/images/categories/chinese.jpg",
+    "일식": "/images/categories/japanese.jpg",
+    "양식": "/images/categories/western.jpg",
+    "카페": "/images/categories/cafe.jpg",
+    "패스트푸드": "/images/categories/fastfood.jpg",
+    "기타": "/images/categories/etc.jpg",
+  };
